658f6d0cb783747d02b8e7a9e742307f93902034
veber <weslusisa@gmail.com>
2022-03-10 15:35:44 -0300

    Melhorando o model User

---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:name) { |n| "User #{n}" }
+    email { Faker::Internet.email }
+    password { "123456"}
+    password_confirmation { "123456"}
+    profile { %i(admin client).sample }
+
+  end
+end
