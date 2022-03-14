25b7767caa11b80e30412e02ae8a912c0239044f
veber <weslusisa@gmail.com>
2022-03-14 11:55:52 -0300

    Adiçao de imagem para Produto

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,5 +6,8 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: {case_sensitive: false}
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :image, presence: true
+
+  has_one_attached :image
 
 end
