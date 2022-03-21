5cf835f8e3ca96ccc165003962e5f9b18df9c058
veber <weslusisa@gmail.com>
2022-03-21 02:13:54 -0300

    Removendo uma Categoria - Testes

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -75,8 +75,6 @@ RSpec.describe "Admin::V1::Categories", type: :request do
         { category: attributes_for(:category, name: new_name) }.to_json
       end
 
-
-
       # it "update as expected name" do
       #   patch url, headers: auth_header(user), params: category_params
       #   expect(body_json['category']['name']).to eq('My new category')
@@ -124,5 +122,42 @@ RSpec.describe "Admin::V1::Categories", type: :request do
       end
 
     end
+  end
+
+  context "DELETE /categories/:id" do
+    let!(:category) { create(:category) }
+    let(:url) { "/admin/v1/categories/#{category.id}" }
+
+    it "removes category" do
+      expect do
+        delete url, headers: auth_header(user)
+      end.to change(Category, :count).by(-1)
+    end
+
+    it "return success status" do
+      delete url, headers: auth_header(user)
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it "doesn't return any body content" do
+      delete url, headers: auth_header(user)
+      expect(body_json).to_not be_present
+    end
+
+    it "removes all associated product categories" do
+      product_categories = create_list(:product_category, 3, category: category)
+      delete url, headers: auth_header(user)
+      expected_product_categories = ProductCategory.where(id: product_categories.map(&:id))
+      expect(expected_product_categories.count).to eq(0)
+    end
+
+    it "doesn't removes unassociated product categories" do
+      product_categories = create_list(:product_category, 3)
+      delete url, headers: auth_header(user)
+      present_product_categories_ids = product_categories.map(&:id)
+      expected_product_categories = ProductCategory.where(id: present_product_categories_ids)
+      expect(expected_product_categories.ids).to contain_exactly(*present_product_categories_ids)
+    end
+
   end
 end
