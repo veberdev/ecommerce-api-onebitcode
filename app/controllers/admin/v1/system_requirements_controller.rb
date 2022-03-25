9a4cffb79e68aea7fd35c5b2577fbfc8d234ce8e
veber <weslusisa@gmail.com>
2022-03-25 18:48:50 -0300

    add crud for system requirements

---
+++ app/controllers/admin/v1/system_requirements_controller.rb
@@ -0,0 +1,50 @@
+module Admin::V1
+  class SystemRequirementsController < ApiController
+
+    before_action :load_system_requirement, only: %i(update destroy)
+
+    def index
+      @system_requirements = SystemRequirement.all
+    end
+
+    def create
+      @system_requirement = SystemRequirement.new
+      @system_requirement.attributes = system_requirement_params
+      save_system_requirement!
+    end
+
+    def update
+      @system_requirement.attributes = system_requirement_params
+      save_system_requirement!
+    end
+
+    def destroy
+      system_requirement_destroy!
+    end
+
+    private
+
+    def save_system_requirement!
+      @system_requirement.save!
+      render :show
+    rescue
+      render_error(fields: @system_requirement.errors.messages)
+    end
+
+    def system_requirement_params
+      return {} unless params.has_key?(:system_requirement)
+      params.require(:system_requirement).permit(:name, :operational_system, :storage, :processor, :memory, :video_board)
+    end
+
+    def system_requirement_destroy!
+      @system_requirement.destroy!
+    rescue
+      render_error(@system_requirement.errors.messages)
+    end
+
+    def load_system_requirement
+      @system_requirement = SystemRequirement.find(params[:id])
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get "home" => "home#index"
       resources :categories
       resources :coupons
+      resources :system_requirements
     end
   end
 
