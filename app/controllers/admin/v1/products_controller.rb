b5f625e639510c1ccfb35d2d7a6c6b6c811e03e2
veber <weslusisa@gmail.com>
2022-04-07 11:28:45 -0300

    Adicionando Featured em Produtos

--- app/controllers/admin/v1/products_controller.rb
+++ app/controllers/admin/v1/products_controller.rb
@@ -46,7 +46,8 @@ module Admin::V1
 
     def product_params
       return {} unless params.has_key?(:product)
-      permitted_params = params.require(:product).permit(:id, :name, :description, :image, :price, :productable, :status, category_ids: [])
+      permitted_params = params.require(:product).permit(:id, :name, :description, :image, :price, :productable,
+                                                         :status, :featured, category_ids: [])
       permitted_params.merge(productable_params)
     end
 
